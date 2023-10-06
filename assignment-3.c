a3aac68a29bd6be44ee763e42ce50f8bc65ee225
Sambit <thewhitedevil@32gmail.com>
2023-10-06 20:41:48 +0530

    New Assignment added

---
+++ assignment-3.c
@@ -0,0 +1,39 @@
+// 1 -->  WAP to find out the sum of first 10 numbers.
+
+/*#include <stdio.h>
+int main()
+{
+    int i, sum = 0;
+    for (i = 1; i <= 10; i++)
+        sum = sum + i;
+    printf("\nThe sum is %d", sum);
+    return 0;
+}*/
+
+// 2 -->  WAP to calculate the sum of the numbers from m to n where m and n will be inputted by the user
+
+/*#include <stdio.h>
+int main()
+{
+    int i, sum = 0, m, n;
+    printf("\nEnter the lower limit (m) and upper limit (n): \n");
+    scanf("%d%d", &m, &n);
+    for (i = m; i <= n; i++)
+        sum = sum + i;
+    printf("Sum = %d\n", sum);
+    return 0;
+}*/
+
+// 3 --> WAP to print the multiplication table of any inputted number
+
+/*#include <stdio.h>
+int main()
+{
+    int i, n;
+    printf("\nEnter a number: ");
+    scanf("%d", &n);
+    printf("\nThe multiplication table is %d is -->", n);
+    for (i = 1; i <= 10; ++i)
+        printf("\n%d x %d = %d", n, i, n * i);
+    return 0;
+}*/
---
+++ cgpa.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    a = -3 - -3;
+    b = -3 - -(-3);
+    printf("a = %d and b = %d", a, b);
+    return 0;
+}
