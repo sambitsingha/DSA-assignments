e35c4f0ccbe42ef08603373220983bf88d820101
Sambit <110420002+sambitsingha@users.noreply.github.com>
2024-02-26 14:50:16 +0530

    Refactor insertion sort implementation

--- sort_search.c
+++ sort_search.c
@@ -202,39 +202,39 @@ int main()
 
 // 6 - Insertion Sort
 
-/*#include <stdio.h>
-void sort(int [],int);
+#include <stdio.h>
+void sort(int[], int);
 int main()
 {
-	int arr[20],n,i;
+	int arr[20], n, i;
 	printf("Enter size: ");
-	scanf("%d",&n);
-	for(i=0;i<n;i++)
+	scanf("%d", &n);
+	for (i = 0; i < n; i++)
 	{
-		printf("Enter element %d: ",i+1);
-		scanf("%d",&arr[i]);
+		printf("Enter element %d: ", i + 1);
+		scanf("%d", &arr[i]);
 	}
-	sort(arr,n);
+	sort(arr, n);
 	printf("Sorted array is: \n");
-	for (i=0;i<n;i++)
+	for (i = 0; i < n; i++)
 	{
-		printf("%d\t",arr[i]);
+		printf("%d\t", arr[i]);
 	}
 	return 0;
 }
 
 void sort(int arr[], int n)
 {
-	int i,j,key;
-	for(i=1;i<n;i++)
+	int i, j, key;
+	for (i = 1; i < n; i++)
 	{
 		key = arr[i];
 		j = i - 1;
-		while(j>=0 && key<arr[j])
+		while (j >= 0 && key < arr[j])
 		{
-			arr[j+1] = arr[j];
+			arr[j + 1] = arr[j];
 			j--;
 		}
-		arr[j+1] = key;
+		arr[j + 1] = key;
 	}
-}*/
+}
---
+++ sparse.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+#define MAX_SIZE 100
+
+typedef struct
+{
+    int row;
+    int col;
+    int value;
+} Triplet;
+
+void findSparseTriplet(int matrix[MAX_SIZE][MAX_SIZE], int rows, int cols, Triplet triplets[], int *numTriplets)
+{
+    int count = 0;
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            if (matrix[i][j] != 0)
+            {
+                triplets[count].row = i;
+                triplets[count].col = j;
+                triplets[count].value = matrix[i][j];
+                count++;
+            }
+        }
+    }
+
+    *numTriplets = count;
+}
+
+int main()
+{
+    int matrix[MAX_SIZE][MAX_SIZE];
+    int rows, cols;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter the number of columns: ");
+    scanf("%d", &cols);
+
+    printf("Enter the matrix elements:\n");
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    Triplet triplets[MAX_SIZE * MAX_SIZE];
+    int numTriplets = 0;
+    findSparseTriplet(matrix, rows, cols, triplets, &numTriplets);
+
+    printf("Triplet representation of the sparse matrix:\n");
+    for (int i = 0; i < numTriplets; i++)
+    {
+        printf("(%d, %d, %d)\n", triplets[i].row, triplets[i].col, triplets[i].value);
+    }
+
+    return 0;
+}
