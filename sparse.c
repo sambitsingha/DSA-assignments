70557f78acec92493d1ef28e54bd56a67d459f0f
Sambit <110420002+sambitsingha@users.noreply.github.com>
2024-03-10 22:38:25 +0530

    sparse matrix

--- sparse.c
+++ sparse.c
@@ -1,44 +1,38 @@
+// 1  - 1.	WAP to find out the Triplet representation of any inputted Sparse matrix.
 /*#include <stdio.h>
 #define MAX_SIZE 100
-typedef struct
+void findTriplets(int matrix[MAX_SIZE][MAX_SIZE], int rows, int cols)
 {
-    int row;
-    int col;
-    int value;
-} Triplet;
-
-void findSparseTriplet(int matrix[MAX_SIZE][MAX_SIZE], int rows, int cols, Triplet triplets[], int *numTriplets)
-{
+    int triplets[MAX_SIZE][3];
     int count = 0;
-
+    // Traverse the matrix and find non-zero elements
     for (int i = 0; i < rows; i++)
     {
         for (int j = 0; j < cols; j++)
         {
             if (matrix[i][j] != 0)
             {
-                triplets[count].row = i;
-                triplets[count].col = j;
-                triplets[count].value = matrix[i][j];
+                triplets[count][0] = i;
+                triplets[count][1] = j;
+                triplets[count][2] = matrix[i][j];
                 count++;
             }
         }
     }
-
-    *numTriplets = count;
+    // Print the triplets
+    printf("Triplet representation of the sparse matrix:\n");
+    for (int i = 0; i < count; i++)
+    {
+        printf("(%d, %d, %d)\n", triplets[i][0], triplets[i][1], triplets[i][2]);
+    }
 }
-
 int main()
 {
     int matrix[MAX_SIZE][MAX_SIZE];
     int rows, cols;
-
-    printf("Enter the number of rows: ");
-    scanf("%d", &rows);
-    printf("Enter the number of columns: ");
-    scanf("%d", &cols);
-
-    printf("Enter the matrix elements:\n");
+    printf("Enter the number of rows and columns of the matrix: ");
+    scanf("%d %d", &rows, &cols);
+    printf("Enter the elements of the matrix:\n");
     for (int i = 0; i < rows; i++)
     {
         for (int j = 0; j < cols; j++)
@@ -46,16 +40,9 @@ int main()
             scanf("%d", &matrix[i][j]);
         }
     }
-
-    Triplet triplets[MAX_SIZE * MAX_SIZE];
-    int numTriplets = 0;
-    findSparseTriplet(matrix, rows, cols, triplets, &numTriplets);
-
-    printf("Triplet representation of the sparse matrix:\n");
-    for (int i = 0; i < numTriplets; i++)
-    {
-        printf("(%d, %d, %d)\n", triplets[i].row, triplets[i].col, triplets[i].value);
-    }
-
+    findTriplets(matrix, rows, cols);
     return 0;
-}*/
+}
+*/
+
+// 2 - 2.	WAP to find out the Transpose of any Sparse matrix. Input the Triplet matrix through the keyboard.
