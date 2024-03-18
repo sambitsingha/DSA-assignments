6cbc5431b26f1824cff2410afec2b8db0a09c0ce
Sambit <110420002+sambitsingha@users.noreply.github.com>
2024-03-18 22:33:17 +0530

    Refactor queue implementation and add circular queue

--- queue.c
+++ queue.c
@@ -1,34 +1,39 @@
 // 1.Write a menu-driven program to perform the Insert, Delete, and Traverse operations on a Queue implemented using an array.
 
-#include <stdio.h>
+/*#include <stdio.h>
 #include <stdlib.h>
 void qinsert();
 void qdelete();
 void qdisplay();
-int q[20], front = -1 , rear = -1,max_size = 5;
+int q[20], front = -1, rear = -1, max_size = 5;
 
 void main()
 {
 	int ch;
-	while(1)
+	while (1)
 	{
 		printf("\n1. Queue Insert");
 		printf("\n2. Queue delete");
 		printf("\n3. Queue display");
 		printf("\n4. Exit");
 		printf("\nEnter choice: ");
-		scanf("%d",&ch);
-		switch(ch)
+		scanf("%d", &ch);
+		switch (ch)
 		{
-			case 1: qinsert();
-				break;
-			case 2: qdelete();
-				break;
-			case 3: qdisplay();
-				break;
-			case 4: exit(0);
-				break;
-			default: printf("\nInvalid choice");
+		case 1:
+			qinsert();
+			break;
+		case 2:
+			qdelete();
+			break;
+		case 3:
+			qdisplay();
+			break;
+		case 4:
+			exit(0);
+			break;
+		default:
+			printf("\nInvalid choice");
 		}
 	}
 }
@@ -36,13 +41,13 @@ void main()
 void qinsert()
 {
 	int item;
-	if(rear == max_size - 1)
+	if (rear == max_size - 1)
 		printf("\nQueue overflow");
 	else
 	{
 		printf("\nEnter value to be inserted: ");
-		scanf("%d",&item);
-		if(front == -1)
+		scanf("%d", &item);
+		if (front == -1)
 		{
 			front = 0;
 			rear = 0;
@@ -55,15 +60,15 @@ void qinsert()
 void qdelete()
 {
 	int item;
-	if(front == -1)
+	if (front == -1)
 		printf("Queue underflow");
 	else
 	{
 		item = q[front];
-		printf("\nValue delete = %d",item);
-		if(front == rear)
+		printf("\nValue delete = %d", item);
+		if (front == rear)
 		{
-			front  = -1;
+			front = -1;
 			rear = -1;
 		}
 		else
@@ -73,11 +78,115 @@ void qdelete()
 void qdisplay()
 {
 	int i;
-	if(front == -1)
+	if (front == -1)
 		printf("\nNo elements in Queue");
 	else
 	{
-		for(i = front;i<=rear;i++)
-		printf("%d\t",q[i]);
+		for (i = front; i <= rear; i++)
+			printf("%d\t", q[i]);
+	}
+}*/
+
+// 2 -  Program to write Circular Queue
+
+/*#include <stdio.h>
+#include <stdlib.h>
+void cqinsert();
+void cqdelete();
+void cqdisplay();
+int cq[20], front = -1, rear = -1, max_size = 5;
+void main()
+{
+	int ch;
+	while (1)
+	{
+		printf("\n1. Circular Queue Insert");
+		printf("\n2. Circular Queue delete");
+		printf("\n3. Circular Queue display");
+		printf("\n4. Exit");
+		printf("\nEnter choice: ");
+		scanf("%d", &ch);
+		switch (ch)
+		{
+		case 1:
+			cqinsert();
+			break;
+		case 2:
+			cqdelete();
+			break;
+		case 3:
+			cqdisplay();
+			break;
+		case 4:
+			exit(0);
+			break;
+		default:
+			printf("\nInvalid choice");
+		}
 	}
 }
+
+void cqinsert()
+{
+	int item;
+	if ((front == 0 && rear == max_size - 1) || (front == rear + 1))
+		printf("\nQueue overflow");
+	else
+	{
+		printf("\nEnter value to be inserted: ");
+		scanf("%d", &item);
+		if (front == -1)
+		{
+			front = 0;
+			rear = 0;
+		}
+		else if (rear == max_size - 1)
+			rear = 0;
+		else
+			rear++;
+		cq[rear] = item;
+	}
+}
+
+void cqdelete()
+{
+	int item;
+	if (front == -1)
+		printf("Queue underflow");
+	else
+	{
+		item = cq[front];
+		printf("\nValue delete = %d", item);
+		if (front == rear)
+		{
+			front = -1;
+			rear = -1;
+		}
+		else if (front == max_size - 1)
+			front = 0;
+		else
+			front++;
+	}
+}
+
+void cqdisplay()
+{
+	int i;
+	if (front == -1)
+		printf("\nNo elements in Queue");
+	else
+	{
+		if (front <= rear)
+		{
+			for (i = front; i <= rear; i++)
+				printf("%d\t", cq[i]);
+		}
+		else
+		{
+			for (i = front; i <= max_size - 1; i++)
+				printf("%d\t", cq[i]);
+			for (i = 0; i <= rear; i++)
+				printf("%d\t", cq[i]);
+		}
+	}
+}*/
