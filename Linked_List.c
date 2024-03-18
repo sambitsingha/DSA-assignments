744a6c5782321a3431e65b05d3b1306c38d9aaec
Sambit <110420002+sambitsingha@users.noreply.github.com>
2024-03-18 22:50:02 +0530

    Linked List code modified

--- Linked_List.c
+++ Linked_List.c
@@ -1,5 +1,5 @@
 // 1 - All linked list operations
-#include <stdio.h>
+/*#include <stdio.h>
 #include <stdlib.h>
 struct node
 {
@@ -7,13 +7,16 @@ struct node
     struct node *link;
 };
 void append(struct node **, int);
-void insert(struct node **, int, int);
-void delete(struct node **, int);
+void insertBeg(struct node **, int);
+void insertEnd(struct node **, int);
+void insertPos(struct node **, int, int);
+void deleteBeg(struct node **);
+void deleteEnd(struct node **);
+void deletePos(struct node **, int);
 void display(struct node *);
-void main()
+int main()
 {
     int i, ch, n, pos, e;
-    system("cls");
     struct node *p = NULL;
     printf("\nEnter no of elements to create a list: ");
     scanf("%d", &n);
@@ -25,35 +28,57 @@ void main()
     }
     while (1)
     {
-        printf("\n1. Insert");
-        printf("\n2.Delete");
-        printf("\n3. Display");
-        printf("\n4.Exit");
+        printf("\n1. Insert at beginning");
+        printf("\n2. Insert at end");
+        printf("\n3.Insert at any postion");
+        printf("\n4. Delete at beginning");
+        printf("\n5. Delete at end");
+        printf("\n6.Delete at any postion");
+        printf("\n7. Display");
+        printf("\n8.Exit");
         printf("\nEnter choice: ");
         scanf("%d", &ch);
         switch (ch)
         {
         case 1:
-            printf("\nEnter value to be inserted: ");
+            printf("\nEnter element: ");
             scanf("%d", &e);
-            printf("\nEnter the position: ");
-            scanf("%d", &pos);
-            insert(&p, pos, e);
+            insertBeg(&p, e);
             break;
         case 2:
-            printf("\nEnter position from where element is deleted: ");
-            scanf("%d", &pos);
-            delete (&p, pos);
+            printf("\nEnter element: ");
+            scanf("%d", &e);
+            insertEnd(&p, e);
+            break;
         case 3:
-            display(p);
+            printf("\nEnter element: ");
+            scanf("%d", &e);
+            printf("\nEnter position: ");
+            scanf("%d", &pos);
+            insertPos(&p, pos, e);
             break;
         case 4:
-            exit(0);
-        default:
-            printf("\nInvalid Choice");
+            deleteBeg(&p);
             break;
+        case 5:
+            deleteEnd(&p);
+            break;
+        case 6:
+            printf("\nEnter position: ");
+            scanf("%d", &pos);
+            deletePos(&p, pos);
+            break;
+        case 7:
+            display(p);
+            break;
+        case 8:
+            exit(0);
+            break;
+        default:
+            printf("\nInvalid choice");
         }
     }
+    return 0;
 }
 void append(struct node **start, int num)
 {
@@ -76,12 +101,19 @@ void append(struct node **start, int num)
         temp->link = r;
     }
 }
-void insert(struct node **start, int loc, int num)
+void insertBeg(struct node **start, int num)
+{
+    struct node *temp;
+    temp = (struct node *)malloc(sizeof(struct node));
+    temp->data = num;
+    temp->link = *start;
+    *start = temp;
+}
+void insertEnd(struct node **start, int num)
 {
     struct node *temp, *r;
-    int j;
     temp = *start;
-    if (loc == 1)
+    if (*start == NULL)
     {
         temp = (struct node *)malloc(sizeof(struct node));
         temp->data = num;
@@ -90,25 +122,95 @@ void insert(struct node **start, int loc, int num)
     }
     else
     {
-        for (j = 1; j < loc - 1; j++)
+        while (temp->link != NULL)
             temp = temp->link;
-        if (temp != NULL)
-            r = (struct node *)malloc(sizeof(struct node));
+        r = (struct node *)malloc(sizeof(struct node));
         r->data = num;
+        r->link = NULL;
+        temp->link = r;
+    }
+}
+void insertPos(struct node **start, int loc, int num)
+{
+    struct node *temp, *r;
+    int i;
+    if (loc < 1)
+    {
+        printf("Invalid location. Location should be greater than 0.\n");
+        return;
+    }
+
+    r = (struct node *)malloc(sizeof(struct node));
+    r->data = num;
+
+    if (loc == 1)
+    {
+        r->link = *start;
+        *start = r;
+    }
+    else
+    {
+        temp = *start;
+        for (i = 1; i < loc - 1; i++)
+        {
+            if (temp == NULL)
+            {
+                printf("Invalid location. Location is greater than the length of the list.\n");
+                return;
+            }
+            temp = temp->link;
+        }
         r->link = temp->link;
         temp->link = r;
     }
 }
-void delete(struct node **start, int loc)
+void deleteBeg(struct node **start)
+{
+    struct node *temp;
+    temp = *start;
+    if (*start != NULL)
+    {
+        *start = temp->link;
+        free(temp);
+    }
+}
+void deleteEnd(struct node **start)
+{
+    struct node *temp, *prev;
+    temp = *start;
+    if (temp->link == NULL)
+    {
+        *start = NULL;
+        free(temp);
+    }
+    else
+    {
+        while (temp->link != NULL)
+        {
+            prev = temp;
+            temp = temp->link;
+        }
+        printf("\nValue deleted = %d", temp->data);
+        prev->link = NULL;
+        free(temp);
+    }
+}
+void deletePos(struct node **start, int loc)
 {
     struct node *temp, *prev;
     int j;
+    if (loc < 1)
+    {
+        printf("Invalid location. Location should be greater than 0.\n");
+        return;
+    }
+
     temp = *start;
     prev = NULL;
+
     if (loc == 1)
     {
-        printf("\nVallue deleted: %d");
-        temp->data;
+        printf("\nValue deleted: %d", temp->data);
         *start = temp->link;
         free(temp);
     }
@@ -116,6 +218,11 @@ void delete(struct node **start, int loc)
     {
         for (j = 1; j < loc; j++)
         {
+            if (temp == NULL)
+            {
+                printf("Invalid location. Location is greater than the length of the list.\n");
+                return;
+            }
             prev = temp;
             temp = temp->link;
         }
@@ -136,4 +243,4 @@ void display(struct node *start)
         printf("%d\t", temp->data);
         temp = temp->link;
     }
-}
+}*/
